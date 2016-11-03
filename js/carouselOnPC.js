5d64dc7e944c0751f04b761c5b31e12e35c1f155
jm365 <270279036@qq.com>
2016-11-03 18:16:15 +0800

    加了点内容
    加了点内容

--- js/carouselOnPC.js
+++ js/carouselOnPC.js
@@ -1,3 +1,13 @@
 ;(function($){
-	
+	var timer;
+	function autoPlay(){
+		console.log(2);
+	};
+
+	$.fn.extend({
+		carouselOnPC:function(){
+			console.log(1);
+			// timer=setInterval()
+		}
+	});
 })(jQuery);
